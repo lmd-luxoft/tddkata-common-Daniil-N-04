945a92cee14e4f8f8c300519b74a94d0d3bb58fe
Daniil_Novokhatniy <daniil212504@gmail.com>
2021-04-13 14:50:11 +0300

    update

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import static java.util.Objects.isNull;
 
 public class Calc {
+    private String[] numbers;
 
     public int sum(String expression) {
         if (isNull(expression))
@@ -10,10 +11,18 @@ public class Calc {
         else if (expression.isEmpty())
             return 0;
 
-        String[] numbers = expression.split("\\,");
-        if (numbers.length == 1)
-            return -1;
+        numbers = expression.split("\\,");
 
-        return Arrays.stream(numbers).map(Integer::parseInt).mapToInt(n -> n).sum();
+        return numbers.length == 1
+                ? -1
+                : getSum();
+    }
+
+    private int getSum() {
+        try {
+            return Arrays.stream(numbers).map(Integer::parseInt).mapToInt(n -> n).sum();
+        } catch (NumberFormatException e) {
+            return -1;
+        }
     }
 }

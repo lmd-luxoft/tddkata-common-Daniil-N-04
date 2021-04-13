8ca542ddfeb9c75f93a668032542855b12c4829f
Daniil_Novokhatniy <daniil212504@gmail.com>
2021-04-13 13:02:34 +0300

    tests

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -1,2 +1,30 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalcTest {
+
+    @Test
+    public void test_sum() {
+        Calc calc = new Calc();
+        int actual = calc.sum("");
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test_sum1_2() {
+        Calc calc = new Calc();
+        int actual = calc.sum("1,2");
+        int expected = 3;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test_sum1_1() {
+        Calc calc = new Calc();
+        int actual = calc.sum("1,1");
+        int expected = 2;
+        assertEquals(expected, actual);
+    }
 }

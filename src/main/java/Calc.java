4de1b2e2f04e527473a2802d135ce76ee58bacf2
Daniil_Novokhatniy <daniil212504@gmail.com>
2021-04-14 20:56:18 +0300

    update

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,43 +1,64 @@
 import java.util.Arrays;
+import java.util.List;
 
 import static java.util.Objects.isNull;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 public class Calc {
+    private static final int ERROR = -1;
     private String[] numbers;
     private String exp;
 
     public int sum(String expression) {
-        this.exp = expression;
+        exp = expression;
         if (isNull(exp))
-            return -1;
-        else if (exp.isEmpty())
+            return ERROR;
+        if (exp.isEmpty())
             return 0;
 
         String separator = getSeparator();
-        numbers = separator.isEmpty() ? exp.split("\\,|\n") : exp.split(separator);
+        numbers = exp.split(separator);
 
-        return numbers.length == 1
-                ? -1
-                : getSum();
+        return getSum();
     }
 
     private int getSum() {
+        if (numbers.length == 1)
+            return ERROR;
+
         try {
-            return Arrays.stream(numbers).map(Integer::parseInt).mapToInt(n -> n).sum();
+            List<Integer> list = Arrays.stream(numbers).mapToInt(Integer::parseInt).boxed().collect(toList());
+            checkNegativeNumbers(list);
+            return list.stream().mapToInt(n -> n).sum();
         } catch (NumberFormatException e) {
-            return -1;
+            return ERROR;
         }
     }
 
     private String getSeparator() {
         String separator = "";
         if (exp.startsWith("//")) {
-            String[] customSeparator = exp.substring(0,4).split("\n");
-            if (customSeparator.length == 1 && customSeparator[0].matches("//.")) {
-                exp = exp.substring(4);
-                separator = customSeparator[0].substring(2,3);
+            String[] customSeparator = exp.split("\n");
+            if (customSeparator.length > 1 && customSeparator[0].matches("//.+")) {
+                exp = exp.substring(customSeparator[0].length() + 1);
+                separator = customSeparator[0].substring(2);
             }
         }
-        return separator;
+        return separator.isEmpty() ? ",|\n" : separator;
+    }
+
+    private void checkNegativeNumbers(List<Integer> list) {
+        List<Integer> negativeNumbers = list.stream().filter(number -> number < 0).collect(toList());
+        if (!negativeNumbers.isEmpty())
+            throw new CustomNumberFormatError("Negatives are not allowed! Negative: " +
+                    negativeNumbers.stream().map(String::valueOf).collect(joining(",")));
+    }
+
+    public static class CustomNumberFormatError extends Error {
+
+        CustomNumberFormatError(String message) {
+            super(message);
+        }
     }
 }
--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -1,168 +1,168 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalcTest {
+    Calc calc;
 
-    @Test
-    public void newSeparator() {
-        Calc calc = new Calc();
-        int actual = calc.sum("2\n5");
-        int expected = 7;
-        assertEquals(expected, actual);
+    @BeforeEach
+    void setUp(){
+        calc = new Calc();
     }
 
-    @Test
-    public void newAndOld_Separator() {
-        Calc calc = new Calc();
-        int actual = calc.sum("2\n5,1");
-        int expected = 8;
-        assertEquals(expected, actual);
+    @Nested
+    class Positive {
+
+        @Test
+        void sum_lineSeparator() {
+            int actual = calc.sum("2\n5");
+            int expected = 7;
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void sum_defaultSeparators() {
+            int actual = calc.sum("2\n5,1");
+            int expected = 8;
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void sum_emptyExpression() {
+            int actual = calc.sum("");
+            int expected = 0;
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void sum_twoDifferentDigitsSeparatedByComma() {
+            int actual = calc.sum("1,2");
+            int expected = 3;
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void sum_twoDigitsWithCommaSeparator() {
+            int actual = calc.sum("1,1");
+            int expected = 2;
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void sum_threeDigitsSeparatedByComma() {
+            int actual = calc.sum("1,1,5");
+            int expected = 7;
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void sum_customSeparate() {
+            int actual = calc.sum("//;\n1;2");
+            int expected = 3;
+            assertEquals(expected, actual);
+        }
     }
 
-    @Test
-    public void emptyExpression() {
-        Calc calc = new Calc();
-        int actual = calc.sum("");
-        int expected = 0;
-        assertEquals(expected, actual);
-    }
+    @Nested
+    class Negative {
 
-    @Test
-    public void sum_twoNumbers1() {
-        Calc calc = new Calc();
-        int actual = calc.sum("1,2");
-        int expected = 3;
-        assertEquals(expected, actual);
-    }
+        @Test
+        void sum_floatNumbers() {
+            int actual = calc.sum("1.1,2.6");
+            int expected = -1;
+            assertEquals(expected, actual);
+        }
 
-    @Test
-    public void sum_twoNumbers2() {
-        Calc calc = new Calc();
-        int actual = calc.sum("1,1");
-        int expected = 2;
-        assertEquals(expected, actual);
-    }
+        @Test
+        void sum_whiteSpaceSeparator() {
+            int actual = calc.sum("1 3");
+            int expected = -1;
+            assertEquals(expected, actual);
+        }
 
-    @Test
-    public void sum_threeNumbers() {
-        Calc calc = new Calc();
-        int actual = calc.sum("1,1,5");
-        int expected = 7;
-        assertEquals(expected, actual);
-    }
+        @Test
+        void sum_oneWordExpression() {
+            int actual = calc.sum("один");
+            int expected = -1;
+            assertEquals(expected, actual);
+        }
 
-    @Test
-    public void sum_twoRealNumbers() {
-        Calc calc = new Calc();
-        int actual = calc.sum("1.1,2.6");
-        int expected = -1;
-        assertEquals(expected, actual);
-    }
+        @Test
+        void sum_oneDigit() {
+            int actual = calc.sum("1");
+            int expected = -1;
+            assertEquals(expected, actual);
+        }
 
-    @Test
-    public void sum_twoNumbers_negative() {
-        Calc calc = new Calc();
-        int actual = calc.sum("1 3");
-        int expected = -1;
-        assertEquals(expected, actual);
-    }
+        @Test
+        void sum_lineWithCommaSeparator() {
+            int actual = calc.sum("2\n,5");
+            int expected = -1;
+            assertEquals(expected, actual);
+        }
 
-    @Test
-    public void invalidExpression1() {
-        Calc calc = new Calc();
-        int actual = calc.sum("один");
-        int expected = -1;
-        assertEquals(expected, actual);
-    }
+        @Test
+        void sum_commaWithLineSeparator() {
+            int actual = calc.sum("2,\n5");
+            int expected = -1;
+            assertEquals(expected, actual);
+        }
 
-    @Test
-    public void invalidExpression2() {
-        Calc calc = new Calc();
-        int actual = calc.sum("1");
-        int expected = -1;
-        assertEquals(expected, actual);
-    }
+        @Test
+        void sum_doubleCommaSeparator() {
+            int actual = calc.sum("2,,5");
+            int expected = -1;
+            assertEquals(expected, actual);
+        }
 
-    @Test
-    public void invalidExpression3() {
-        Calc calc = new Calc();
-        int actual = calc.sum("2\n,5");
-        int expected = -1;
-        assertEquals(expected, actual);
-    }
+        @Test
+        void sum_doubleLineSeparator() {
+            int actual = calc.sum("2\n\n5");
+            int expected = -1;
+            assertEquals(expected, actual);
+        }
 
-    @Test
-    public void invalidExpression4() {
-        Calc calc = new Calc();
-        int actual = calc.sum("2,\n5");
-        int expected = -1;
-        assertEquals(expected, actual);
-    }
+        @Test
+        void sum_nullExpression() {
+            int actual = calc.sum(null);
+            int expected = -1;
+            assertEquals(expected, actual);
+        }
 
-    @Test
-    public void invalidExpression5() {
-        Calc calc = new Calc();
-        int actual = calc.sum("2,,5");
-        int expected = -1;
-        assertEquals(expected, actual);
-    }
+        @Test
+        void sum_notUsedCustomSeparator() {
+            int actual = calc.sum("//;\n1\n2");
+            int expected = -1;
+            assertEquals(expected, actual);
+        }
 
-    @Test
-    public void invalidExpression6() {
-        Calc calc = new Calc();
-        int actual = calc.sum("2\n\n5");
-        int expected = -1;
-        assertEquals(expected, actual);
-    }
+        @Test
+        void sum_incorrectlySetCustomSeparator() {
+            int actual = calc.sum("//;1;2");
+            int expected = -1;
+            assertEquals(expected, actual);
+        }
 
-    @Test
-    public void nullExpression() {
-        Calc calc = new Calc();
-        int actual = calc.sum(null);
-        int expected = -1;
-        assertEquals(expected, actual);
-    }
+        @Test
+        void sum_invalidExpression() {
+            int actual = calc.sum("//\n\n1\n2");
+            int expected = -1;
+            assertEquals(expected, actual);
+        }
 
-    @Test
-    public void newSeparator1() {
-        Calc calc = new Calc();
-        int actual = calc.sum("//;\n1;2");
-        int expected = 3;
-        assertEquals(expected, actual);
-    }
+        @Test
+        void sum_negativeDigitByCustomSeparator() {
+            Throwable throwable = assertThrows(Calc.CustomNumberFormatError.class, () -> calc.sum("//;\n-1;2"));
+            assertEquals("Negatives are not allowed! Negative: -1", throwable.getMessage());
+        }
 
-    @Test
-    public void invalidExpression7() {
-        Calc calc = new Calc();
-        int actual = calc.sum("//;\n1\n2");
-        int expected = -1;
-        assertEquals(expected, actual);
+        @Test
+        void sum_negativeDigitsByCommaSeparator() {
+            Throwable throwable = assertThrows(Calc.CustomNumberFormatError.class, () -> calc.sum("-1,-2"));
+            assertEquals("Negatives are not allowed! Negative: -1,-2", throwable.getMessage());
+        }
     }
-
-    @Test
-    public void invalidExpression8() {
-        Calc calc = new Calc();
-        int actual = calc.sum("//;1;2");
-        int expected = -1;
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void invalidExpression9() {
-        Calc calc = new Calc();
-        assertThrows(NumberFormatException.class
-                , () -> calc.sum("//;-1;2")
-                , "Negatives are not allowed! Negative: -1" );
-    }
-
-    @Test
-    public void invalidExpression10() {
-        Calc calc = new Calc();
-        assertThrows(NumberFormatException.class
-                , () -> calc.sum("-1;-2")
-                , "Negatives are not allowed! Negative: -1,-2" );
-    }
-
 }
